587636a8b02076851a50725f084d31cff601b579
farufensil-creator <farufensil@gmail.com>
2026-07-17 13:39:23 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
+window.onerror = function (message, source, lineno, colno, error) {
+  alert('ERROR: ' + message + '\n' + (error && error.stack ? error.stack : ''));
+};
+window.addEventListener('unhandledrejection', function (event) {
+  alert('PROMISE ERROR: ' + (event.reason && event.reason.message ? event.reason.message : event.reason));
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
